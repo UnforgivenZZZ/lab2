244d33cf81237a1937f45cfe94ffb011536b6f7f
jialiangzhou <ericcareer@hotmail.com>
2016-04-06 11:26:43 -0700

    another newer

--- app/src/main/java/com/android/lab2/MainActivity.java
+++ app/src/main/java/com/android/lab2/MainActivity.java
@@ -18,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //TextView object goes here
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
